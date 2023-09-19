da139e3a8432b7827bda7fe290865fd6c3714f11
seanclaybaugh <69654199+seanclaybaugh@users.noreply.github.com>
2023-09-19 10:47:12 -0700

    Time: 58 ms (24.52%), Space: 41.4 MB (93.65%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    
+    if (haystack.includes(needle)) {
+        return haystack.indexOf(needle)
+    }
+    
+    return -1;
+};
