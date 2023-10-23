0ebe96ea65d4590e0a3f7d97349b4569e4c8faea
seanclaybaugh <69654199+seanclaybaugh@users.noreply.github.com>
2023-10-23 10:40:13 -0700

    Time: 58 ms (18.95%), Space: 41.7 MB (79.47%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    let tally = 0;
+    for (let i = 0; i < arr.length; i ++) {
+        if (arr[i] % 2 === 0) {
+            tally = 0            
+        } else {
+            tally += 1
+            if(tally === 3) {
+                return true
+            }
+        }
+    }
+    return false
+};
