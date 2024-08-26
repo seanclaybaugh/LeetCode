7a57510dfdfd355c7f957143f2b45eb4db199db4
seanclaybaugh <69654199+seanclaybaugh@users.noreply.github.com>
2024-08-26 12:41:30 -0700

    Time: 57 ms (74.49%), Space: 51.5 MB (63.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,22 +3,21 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    const counts = {};
-    nums.forEach((val)=>{
-        if (counts[val]) {
-            counts[val]++
-        } else {
-            counts[val] = 1
-        }
-    })
-    console.log(counts)
-    let max = 0;
-    let result = '';
-    for (key in counts) {
-        if (counts[key] > max) {
-            result = key;
-            max = counts[key];
-        }
+   const key = {};
+   let result = '';
+   let max = 0;
+   nums.forEach((val)=> {
+    if (key[val]) {
+        key[val]++
+    } else {
+        key[val] = 1;
     }
+   })
+   for (prop in key) {
+    if (key[prop] > max) {
+        max = key[prop]
+        result = prop
+    }
+   }
     return result;
 };
