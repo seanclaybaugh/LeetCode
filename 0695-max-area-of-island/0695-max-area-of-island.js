1d67e94ea0ab780c6fe36220ad2900f25c6639fc
seanclaybaugh <69654199+seanclaybaugh@users.noreply.github.com>
2024-10-10 12:35:52 -0700

    Time: 69 ms (68.08%), Space: 52.3 MB (59.48%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+    if (grid.length === 0 || grid[0].length === 0) {
+        return 0;
+    }
+    let max = 0;
+    const h = grid.length
+    const w = grid[0].length
+    
+    const dfs = (i, j) => {
+        
+        if (i < h && j < w && i >= 0 && j >= 0 && grid[i][j] === 1) {
+            grid[i][j] = 0
+            return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j-1) + dfs(i, j+1)
+        }
+        
+        return 0
+    }
+    
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
+            if (grid[i][j] === 1) {
+                max = Math.max(max, dfs(i, j))
+            }
+        }
+    }
+    
+    return max
+}
+   
