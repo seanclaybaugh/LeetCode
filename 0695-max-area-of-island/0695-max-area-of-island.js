6820735b6727941e9970904bbcc32f88a8179491
seanclaybaugh <69654199+seanclaybaugh@users.noreply.github.com>
2024-10-11 12:36:22 -0700

    Time: 67 ms (75.28%), Space: 50.9 MB (99.38%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -16,7 +16,6 @@ var maxAreaOfIsland = function(grid) {
             grid[i][j] = 0
             return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j-1) + dfs(i, j+1)
         }
-        
         return 0
     }
     
